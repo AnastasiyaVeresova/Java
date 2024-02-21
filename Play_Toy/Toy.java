1dd04cba29c8f8307068033b64c9b9c651d39855
AV <nastulua@yandex.ru>
2024-02-21 07:49:10 +0300

    PriorityQueue

--- Play_Toy/Toy.java
+++ Play_Toy/Toy.java
@@ -1,18 +1,4 @@
 package Play_Toy;
-//java.util.PriorityQueue;
-//public class Toy {
-//    private int id;
-//    private String name;
-//    private int weight;
-//
-//    public Toy(int id, String name, int weight) {
-//        this.id = id;
-//        this.name = name;
-//        this.weight = weight;
-//    }
-//
-//
-//}
 
 import java.io.BufferedWriter;
 import java.nio.charset.StandardCharsets;
@@ -20,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.PriorityQueue;
 
 public class Toy {
     private int id;
@@ -31,19 +18,25 @@ public class Toy {
         this.name = name;
         this.weight = weight;
     }
+
     public static void main(String[] args) throws Exception {
+        int[] ids = {1, 2, 3};
+        int[] frequencies = {3, 2, 6};
+        String[] names = {"Toy 1", "Toy 2", "Toy 3"};
+
+        PriorityQueue<Toy> toy = new PriorityQueue<>();
+        for (int i = 0; i < ids.length; i++) {
+            toy.add(new Toy(ids[i], names[i], frequencies[i]));
+        }
 
         Path logFile = Paths.get("toy_weight.log");
         if (Files.notExists(logFile)) {
             Files.createFile(logFile);
         }
 
-        try (BufferedWriter writer =
-                     Files.newBufferedWriter(logFile, StandardCharsets.UTF_8,
-                             StandardOpenOption.WRITE)) {
-
-            for (int i = 0; i < 10; i++) {
-                writer.write(String.format("Message %s%n", i));
+        try (BufferedWriter writer = Files.newBufferedWriter(logFile, StandardCharsets.UTF_8, StandardOpenOption.WRITE)) {
+            for (Toy t : toy) {
+                writer.write(String.format("Toy id: %d, Name: %s, Weight: %d%n", t.id, t.name, t.weight));
             }
         } catch (Exception e) {
             e.printStackTrace();
